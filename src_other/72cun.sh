4996f6f24c0777d07816b1a2ee72cf0789a058bb
陆英杰 <34340797+shiniaxiaomi@users.noreply.github.com>
2018-12-26 14:00:21 +0800

    修改shell脚本

--- src_other/72cun.sh
+++ src_other/72cun.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 
 #杀掉进程
-appName=72cun
-pid=$(pgrep -f ${appName} )
-num=0
-for buff in ${pid}
-do
-   num=`expr ${num} + 1`
-done
-
-if [ ${num} == 2 ]
-  then 
-     kill -9 $(pgrep -f ${appName} | sed -n '1p')
-     echo app stopped
-fi
+kill -9 `cat /72cun/72cun.pid`
 
 #启动应用
 path=/72cun
 files=$(ls $path)
 for filename in $files
 do
- app=$(expr index "$filename " 72cun)
-  if [ $app == 1 ]
- then 
-   nohup java -jar /72cun/$filename --spring.profiles.active=prod --server.port=9999 >  /72cun/temp.log &
-   break
+ if [[ $filename == *.jar* ]]
+  then
+     nohup java -jar /72cun/$filename --spring.profiles.active=prod --server.port=9999 > /72cun/temp.log & echo $! > /72cun/72cun.pid
+     break
  fi
 done
 
